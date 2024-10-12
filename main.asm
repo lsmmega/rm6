333dc7339e6aa0e01c05285a220eac94e9bf4f85
lsmmega <pogmegalsm@outlook.com>
2024-10-12 17:38:44 +0800

    link process

--- main.asm
+++ main.asm
@@ -5,3 +5,14 @@ incsrc macros/header.asm
 ;header
 base $0
 incsrc header.asm
+
+;bank 0
+base $0
+pad $10
+incbin gfx/obj/rm/rm.chr
+incbin gfx/obj/wpn/charge_shot.chr
+pad $810
+incbin gfx/obj/bar.chr , $10
+incbin gfx/obj/wpn/mega_buster.chr
+incbin gfx/obj/capsule/small_wpn_capsule.chr
+incbin gfx/obj/tank/e_tank.chr
