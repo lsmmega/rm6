934b45ebeab55ff9490ec47e0dd985fa60f1c7d5
lsmmega <pogmegalsm@outlook.com>
2024-10-12 17:18:01 +0800

    create e-tank

--- build.bat
+++ build.bat
@@ -3,5 +3,6 @@ bmp2nes gfx/obj/wpn/charge_shot.bmp
 bmp2nes gfx/obj/bar.bmp
 bmp2nes gfx/obj/wpn/mega_buster.bmp
 bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
+bmp2nes gfx/obj/tank/e_tank.bmp
 asm6 main.asm rm6.nes
 @pause
