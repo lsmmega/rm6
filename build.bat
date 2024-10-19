e5e9e2ffd9ae1fb483796736fb51c7c5f5d821d8
lsmmega <pogmegalsm@outlook.com>
2024-10-19 22:42:00 +0800

    create small life capsule

--- build.bat
+++ build.bat
@@ -5,5 +5,6 @@ bmp2nes gfx/obj/wpn/mega_buster.bmp
 bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 bmp2nes gfx/obj/tank/e_tank.bmp
 bmp2nes gfx/obj/capsule/large_life_capsule.bmp
+bmp2nes gfx/obj/capsule/small_life_capsule.bmp
 asm6 main.asm rm6.nes
 @pause
