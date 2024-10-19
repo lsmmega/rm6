7d3a2e5aeee3bdbb6bf11befc4b151ee375258d9
lsmmega <pogmegalsm@outlook.com>
2024-10-19 22:11:14 +0800

    add large life capsule

--- build.bat
+++ build.bat
@@ -4,5 +4,6 @@ bmp2nes gfx/obj/bar.bmp
 bmp2nes gfx/obj/wpn/mega_buster.bmp
 bmp2nes gfx/obj/capsule/small_wpn_capsule.bmp
 bmp2nes gfx/obj/tank/e_tank.bmp
+bmp2nes gfx/obj/capsule/large_life_capsule.bmp
 asm6 main.asm rm6.nes
 @pause
