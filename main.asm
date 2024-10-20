ae113bda962294fb671a76b4d093ecc135c72c36
lsmmega <pogmegalsm@outlook.com>
2024-10-20 17:17:17 +0800

    rework

--- main.asm
+++ main.asm
@@ -25,17 +25,17 @@ incbin gfx/sprites/ready.chr
 incbin gfx/sprites/tanks/blank_tank.chr
 incbin gfx/sprites/bars/bar_vertical.chr , $0, $10
 pad $1010
-incbin gfx/sprites/rockman/pow.chr , $10
+incbin gfx/sprites/rockman/pow.chr , $0, $7f0
 pad $1810
-incbin gfx/sprites/rockman/jet.chr , $10
+incbin gfx/sprites/rockman/jet.chr , $0, $7f0
 
 ;bank 1
 base $0
 pad $10
-incbin gfx/sprites/menus/animations/rockman_to_jet.chr , $10
+incbin gfx/sprites/menus/animations/rockman_to_jet.chr , $0, $7f0
 pad $810
-incbin gfx/sprites/menus/animations/rockman_to_pow.chr , $10
+incbin gfx/sprites/menus/animations/rockman_to_pow.chr , $0, $7f0
 pad $1010
-incbin gfx/sprites/menus/animations/jet_to_pow.chr , $10
+incbin gfx/sprites/menus/animations/jet_to_pow.chr , $0, $7f0
 pad $1810
-incbin gfx/sprites/menus/animations/pow_to_jet.chr , $10
+incbin gfx/sprites/menus/animations/pow_to_jet.chr , $0, $7f0
