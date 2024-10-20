0ba9c3ff53cdd1735580f8a2200491f17b80ce31
lsmmega <pogmegalsm@outlook.com>
2024-10-20 11:11:20 +0800

    create blank tank

--- build.bat
+++ build.bat
@@ -10,5 +10,6 @@ bmp2nes gfx/obj/capsule/large_wpn_capsule.bmp
 bmp2nes gfx/obj/killed.bmp
 bmp2nes gfx/obj/1up.bmp
 bmp2nes gfx/obj/ready.bmp
+bmp2nes gfx/obj/tank/blank_tank.bmp
 asm6 main.asm rm6.nes
 @pause
