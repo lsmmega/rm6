4d8a08b18ea3268d8609ec12e14a43aca7311200
lsmmega <pogmegalsm@outlook.com>
2024-10-20 11:57:39 +0800

    link progress

--- main.asm
+++ main.asm
@@ -16,3 +16,15 @@ incbin gfx/obj/bar.chr , $10
 incbin gfx/obj/wpn/mega_buster.chr
 incbin gfx/obj/capsule/small_wpn_capsule.chr
 incbin gfx/obj/tank/e_tank.chr
+incbin gfx/obj/capsule/large_life_capsule.chr
+incbin gfx/obj/capsule/small_life_capsule.chr
+incbin gfx/obj/capsule/large_wpn_capsule.chr
+incbin gfx/obj/killed.chr
+incbin gfx/obj/1up.chr
+incbin gfx/obj/ready.chr
+incbin gfx/obj/tank/blank_tank.chr
+incbin gfx/obj/bar.chr , $0, $10
+pad $1010
+incbin gfx/obj/rm/pow.chr , $10
+pad $1810
+incbin gfx/obj/rm/jet.chr , $10
