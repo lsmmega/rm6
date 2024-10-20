cb380c573110a3763f2ef988eae2613a44e59add
lsmmega <pogmegalsm@outlook.com>
2024-10-20 08:47:43 +0800

    create ready

--- build.bat
+++ build.bat
@@ -9,5 +9,6 @@ bmp2nes gfx/obj/capsule/small_life_capsule.bmp
 bmp2nes gfx/obj/capsule/large_wpn_capsule.bmp
 bmp2nes gfx/obj/killed.bmp
 bmp2nes gfx/obj/1up.bmp
+bmp2nes gfx/obj/ready.bmp
 asm6 main.asm rm6.nes
 @pause
